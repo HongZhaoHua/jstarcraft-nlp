baa1efc46aa2f5dcdd239b24476f80194523d03e
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-10-24 19:43:27 +0800

    HanLP的Lucene插件.兼容Lucene 8

---
+++ src/main/java/com/jstarcraft/nlp/lucene/HanlpIndexAnalyzer.java
@@ -0,0 +1,43 @@
+package com.jstarcraft.nlp.lucene;
+
+import java.util.Set;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+
+import com.hankcs.hanlp.HanLP;
+
+/**
+ * HanLP索引分析器(仅用于索引)
+ * 
+ * @author Birdy
+ *
+ */
+public class HanlpIndexAnalyzer extends Analyzer {
+
+    private String algorithm;
+    
+    private Set<String> filter;
+
+    public HanlpIndexAnalyzer(String algorithm, Set<String> filter) {
+        this.algorithm = algorithm;
+        this.filter = filter;
+    }
+
+    public HanlpIndexAnalyzer(String algorithm) {
+        super();
+        this.algorithm = algorithm;
+    }
+
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName) {
+        Tokenizer from = new HanlpSegmenter(HanLP.newSegment(algorithm).enableIndexMode(true), filter);
+        TokenStream to = new LowerCaseFilter(from);
+        to = new PorterStemFilter(to);
+        return new TokenStreamComponents(from, to);
+    }
+
+}
---
+++ src/main/java/com/jstarcraft/nlp/lucene/HanlpQueryAnalyzer.java
@@ -0,0 +1,46 @@
+package com.jstarcraft.nlp.lucene;
+
+import java.util.Set;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+
+import com.hankcs.hanlp.HanLP;
+
+/**
+ * HanLP查询分析器(仅用于查询)
+ * 
+ * @author Birdy
+ *
+ */
+public class HanlpQueryAnalyzer extends Analyzer {
+
+    private String algorithm;
+
+    private Set<String> filter;
+
+    public HanlpQueryAnalyzer(String algorithm, Set<String> filter) {
+        this.algorithm = algorithm;
+        this.filter = filter;
+    }
+
+    public HanlpQueryAnalyzer(String algorithm) {
+        super();
+        this.algorithm = algorithm;
+    }
+
+    /**
+     * 重载Analyzer接口,构造分词组件
+     */
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName) {
+        Tokenizer from = new HanlpSegmenter(HanLP.newSegment(algorithm).enableOffset(true), filter);
+        TokenStream to = new LowerCaseFilter(from);
+        to = new PorterStemFilter(to);
+        return new TokenStreamComponents(from, to);
+    }
+
+}

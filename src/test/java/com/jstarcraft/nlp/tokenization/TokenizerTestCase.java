e6250f3015c2208b23de3202999406022bc5980f
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-10-31 23:26:44 +0800

    重构分词器单元测试.

--- src/test/java/com/jstarcraft/nlp/tokenization/TokenizerTestCase.java
+++ src/test/java/com/jstarcraft/nlp/tokenization/TokenizerTestCase.java
@@ -15,9 +15,9 @@ public abstract class TokenizerTestCase {
         Iterable<? extends Token> tokens = tokenizer.tokenize(text);
         for (Token token : tokens) {
             buffer.append(token.getTerm());
-//            System.err.println(token.getTerm());
-//            System.err.println(token.getBegin());
-//            System.err.println(token.getEnd());
+            System.out.println(token.getTerm());
+            System.out.println(token.getBegin());
+            System.out.println(token.getEnd());
             Assert.assertEquals(token.getTerm().toLowerCase(), text.substring(token.getBegin(), token.getEnd()).toLowerCase());
         }
 //        Assert.assertEquals(text, buffer.toString());

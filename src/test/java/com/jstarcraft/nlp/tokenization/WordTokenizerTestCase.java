e29eb8b0298784845e1625babb25bf9e01e4d2ae
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-11-02 12:07:52 +0800

    重构Word单元测试.

--- src/test/java/com/jstarcraft/nlp/tokenization/WordTokenizerTestCase.java
+++ src/test/java/com/jstarcraft/nlp/tokenization/WordTokenizerTestCase.java
@@ -3,6 +3,7 @@ package com.jstarcraft.nlp.tokenization;
 import org.apdplat.word.segmentation.Segmentation;
 import org.apdplat.word.segmentation.SegmentationAlgorithm;
 import org.apdplat.word.segmentation.SegmentationFactory;
+import org.apdplat.word.util.WordConfTools;
 
 import com.jstarcraft.nlp.tokenization.word.WordTokenizer;
 
@@ -10,6 +11,10 @@ public class WordTokenizerTestCase extends NlpTokenizerTestCase {
 
     @Override
     protected NlpTokenizer<? extends NlpToken> getTokenizer() {
+        // 保持标点符号
+        WordConfTools.set("keep.punctuation", "true");
+        // 保持空格
+        WordConfTools.set("keep.whitespace", "true");
         Segmentation segmentation = SegmentationFactory.getSegmentation(SegmentationAlgorithm.FullSegmentation);
         return new WordTokenizer(segmentation);
     }

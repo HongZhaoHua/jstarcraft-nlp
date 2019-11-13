38ba780dd1452b97060f3cc19ef37ee294b3c206
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-11-13 18:16:22 +0800

    实体识别.

---
+++ src/main/java/com/jstarcraft/nlp/recognition/NlpEntity.java
@@ -0,0 +1,20 @@
+package com.jstarcraft.nlp.recognition;
+
+/**
+ * 实体
+ * 
+ * @author Birdy
+ *
+ */
+public enum NlpEntity {
+
+    /** 何物/何事 */
+    What,
+    /** 何时 */
+    When,
+    /** 何地 */
+    Where,
+    /** 何人 */
+    Who
+
+}

5a168d3591836399487be6eb20fd01bf049be279
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-03-07 11:51:47 +0800

    识别模式.

--- src/main/java/com/jstarcraft/nlp/extraction/recognition/RecognitionMode.java
+++ src/main/java/com/jstarcraft/nlp/extraction/recognition/RecognitionMode.java
@@ -2,10 +2,10 @@ package com.jstarcraft.nlp.extraction.recognition;
 
 public enum RecognitionMode {
 
-    /** 贪婪 */
+    /** 贪婪(尽可能长) */
     Greedy,
 
-    /** 懒惰 */
+    /** 懒惰(尽可能短) */
     Lazy,
 
 }

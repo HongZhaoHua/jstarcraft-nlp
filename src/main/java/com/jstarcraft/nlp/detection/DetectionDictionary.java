b05d33f13c57e888a39d867c3ce27d663f15d1c8
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-04-10 21:14:17 +0800

    准备支持语种检测的配置.

---
+++ src/main/java/com/jstarcraft/nlp/detection/DetectionDictionary.java
@@ -0,0 +1,21 @@
+package com.jstarcraft.nlp.detection;
+
+import com.jstarcraft.nlp.dictionary.NlpDictionary;
+
+/**
+ * 检测词典
+ * 
+ * @author Birdy
+ *
+ */
+public class DetectionDictionary {
+
+    private String name;
+
+    private NlpDictionary dictionary;
+
+    public void test() {
+
+    }
+
+}
---
+++ src/main/java/com/jstarcraft/nlp/detection/DetectionRegulation.java
@@ -0,0 +1,17 @@
+package com.jstarcraft.nlp.detection;
+
+import java.util.regex.Pattern;
+
+/**
+ * 检测规则
+ * 
+ * @author Birdy
+ *
+ */
+public class DetectionRegulation {
+
+    private String name;
+    
+    private Pattern pattern;
+    
+}

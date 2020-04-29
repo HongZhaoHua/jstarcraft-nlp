ba0ff008d5a82a509b6505d2b5efbcb100bc6e20
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-04-29 10:36:08 +0800

    升级到JUnit 5.

--- src/test/java/com/jstarcraft/nlp/detection/DetectionTestCase.java
+++ src/test/java/com/jstarcraft/nlp/detection/DetectionTestCase.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,7 +25,7 @@ public class DetectionTestCase {
     public void testLoadPattern(String path, int size) {
         try (InputStream stream = DetectionPattern.class.getResourceAsStream(path)) {
             Map<String, DetectionPattern> regulations = DetectionPattern.loadPatterns(stream);
-            Assert.assertEquals(size, regulations.size());
+            Assertions.assertEquals(size, regulations.size());
         } catch (Exception exception) {
             throw new IllegalArgumentException(exception);
         }
@@ -42,7 +42,7 @@ public class DetectionTestCase {
     public void testLoadTrie(String path, int size) {
         try (InputStream stream = DetectionTrie.class.getResourceAsStream(path)) {
             Map<String, Set<DetectionTrie>> dictionaries = DetectionTrie.loadTries(stream);
-            Assert.assertEquals(size, dictionaries.size());
+            Assertions.assertEquals(size, dictionaries.size());
         } catch (Exception exception) {
             throw new IllegalArgumentException(exception);
         }
@@ -54,16 +54,16 @@ public class DetectionTestCase {
     @Test
     public void testKnowedLanguage() {
         LanguageDetector detector = new LanguageDetector(DetectionPattern.LANGUAGE_187, DetectionTrie.LANGUAGE_187);
-        Assert.assertEquals("eng", detector.detectLanguage("The goal is to provide a general Java core programming framework").getLocale().toLanguageTag());
-        Assert.assertEquals("cmn", detector.detectLanguage("目标是提供一个通用的Java核心编程框架").getLocale().toLanguageTag());
-        Assert.assertEquals("fra", detector.detectLanguage("L'objectif est de fournir un cadre de programmation de base commun pour Java").getLocale().toLanguageTag());
-        Assert.assertEquals("spa", detector.detectLanguage("El objetivo es ofrecer un marco común de programación básica Java").getLocale().toLanguageTag());
-        Assert.assertEquals("por", detector.detectLanguage("O objetivo é fornecer um Quadro geral de programação do núcleo Java").getLocale().toLanguageTag());
-        Assert.assertEquals("rus", detector.detectLanguage("Цель заключается в том, чтобы создать общую базу программирования Java").getLocale().toLanguageTag());
-        Assert.assertEquals("arb", detector.detectLanguage("الهدف هو توفير إطار البرمجة جافا الأساسية المشتركة").getLocale().toLanguageTag());
-        Assert.assertEquals("deu", detector.detectLanguage("Das Ziel ist es, einen allgemeinen Java-Core-Programmrahmen bereitzustellen").getLocale().toLanguageTag());
-        Assert.assertEquals("jpn", detector.detectLanguage("目標は汎用Javaコアプログラミングフレームを提供することです。").getLocale().toLanguageTag());
-        Assert.assertEquals("hin", detector.detectLanguage("लक्ष्य यह है कि सामान्य जावा कोर प्रोग्रामिंग फ्रेमवर्क प्रदान करना है").getLocale().toLanguageTag());
+        Assertions.assertEquals("eng", detector.detectLanguage("The goal is to provide a general Java core programming framework").getLocale().toLanguageTag());
+        Assertions.assertEquals("cmn", detector.detectLanguage("目标是提供一个通用的Java核心编程框架").getLocale().toLanguageTag());
+        Assertions.assertEquals("fra", detector.detectLanguage("L'objectif est de fournir un cadre de programmation de base commun pour Java").getLocale().toLanguageTag());
+        Assertions.assertEquals("spa", detector.detectLanguage("El objetivo es ofrecer un marco común de programación básica Java").getLocale().toLanguageTag());
+        Assertions.assertEquals("por", detector.detectLanguage("O objetivo é fornecer um Quadro geral de programação do núcleo Java").getLocale().toLanguageTag());
+        Assertions.assertEquals("rus", detector.detectLanguage("Цель заключается в том, чтобы создать общую базу программирования Java").getLocale().toLanguageTag());
+        Assertions.assertEquals("arb", detector.detectLanguage("الهدف هو توفير إطار البرمجة جافا الأساسية المشتركة").getLocale().toLanguageTag());
+        Assertions.assertEquals("deu", detector.detectLanguage("Das Ziel ist es, einen allgemeinen Java-Core-Programmrahmen bereitzustellen").getLocale().toLanguageTag());
+        Assertions.assertEquals("jpn", detector.detectLanguage("目標は汎用Javaコアプログラミングフレームを提供することです。").getLocale().toLanguageTag());
+        Assertions.assertEquals("hin", detector.detectLanguage("लक्ष्य यह है कि सामान्य जावा कोर प्रोग्रामिंग फ्रेमवर्क प्रदान करना है").getLocale().toLanguageTag());
     }
 
     /**
@@ -77,14 +77,14 @@ public class DetectionTestCase {
             Object2BooleanMap<String> options = new Object2BooleanOpenHashMap<>();
             options.put("eng", true);
             DetectionLanguage language = detector.detectLanguage("目标是提供一个通用的Java核心编程框架", options);
-            Assert.assertNull(language);
+            Assertions.assertNull(language);
         }
         {
             // 指定黑名单
             Object2BooleanMap<String> options = new Object2BooleanOpenHashMap<>();
             options.put("cmn", false);
             DetectionLanguage language = detector.detectLanguage("目标是提供一个通用的Java核心编程框架", options);
-            Assert.assertNull(language);
+            Assertions.assertNull(language);
         }
     }
 
